014eb13e257172cbf7763a570612a729fbbfd039
pma at cluster <patrick.mcallister@asia-europe.uni-heidelberg.de>
2016-05-17 15:17:16 +0200

    hyphenate long words too
    the build_all.sh script should also be working now

--- bin/hyphenate.sh
+++ bin/hyphenate.sh
@@ -1,16 +1,27 @@
 #! /bin/sh
 ":"; exec emacs --quick --script "$0" -- "$@" # -*-emacs-lisp-*-
 
-(defvar vowels ;; (-flatten (first indian-dev-base-table))
+(defvar hyph-hack/vowels ;; (-flatten (first indian-dev-base-table))
   '(2309 2310 2366 2311 2367 2312 2368 2313 2369 2314 2370 2315 2371 2316 2402 2317 2373 2318 2374 2319 2375 2320 2376 2321 2377 2322 2378 2323 2379 2324 2380 2400 2372 2401 2403))
 
-(defvar consonants ;; (butlasat (butlast (-flatten (second indian-dev-base-table))))
+(defvar hyph-hack/long-word-regex
+  ;; (rx-to-string '(>= 60 (syntax word)))
+  "\\(?:\\sw\\{60,\\}\\)")
+
+(defvar hyph-hack/vowels-iast-regex;; not complete
+  ;; (regexp-opt (list "a" "ā" "i" "ī" "u" "ū" "ṛ" "ḷ"))
+  "[aiuāīūḷṛ]")
+
+(defvar hyph-hack/consonants ;; (butlasat (butlast (-flatten (second indian-dev-base-table))))
   '(2325 2326 2327 2328 2329 2330 2331 2332 2333 2334 2335 2336 2337 2338 2339 2340 2341 2342 2343 2344 2345 2346 2347 2348 2349 2350 2351 2352 2353 2354 2355 2356 2357 2358 2359 2360 2361 2392 2393 2394 2395 2396 2397 2398 2399))
 
-(defvar virāma 2381)
-(defvar break-permitted 130)
+(defvar hyph-hack/vowels-and-consonants (cons hyph-hack/vowels hyph-hack/consonants))
 
-(defun hacks/devanāgarī-hyphenate-string (string &optional verify)
+(defvar hyph-hack/virāma 2381)
+
+(defvar hyph-hack/break-permitted 130)
+
+(defun hyph-hack/devanāgarī-hyphenate-string (string &optional verify)
   "Insert character 130 (break permitted here) into STRING.
 
 It effectively splits up unicode devanāgarī strings.  If VERIFY
@@ -23,27 +34,44 @@ devanāgarī."
     (while src
       (setq item (pop src))
       (setq results
-	    (cond
-	     ((and
-	       (memq item consonants)
-	       (memq (car src) consonants))
-	      (cons break-permitted (cons item results)))
-	     ((and
-	       (memq item vowels)
-	       (memq (car src) consonants))
-	      (cons break-permitted (cons item results)))
-	     (t (cons item results)))))
+	    (if (and;; consonant or vowel preceded by a consonant --> allow break
+		 (memq (car src) hyph-hack/consonants)
+		 (memq item hyph-hack/vowels-and-consonants))
+		(cons hyph-hack/break-permitted (cons item results))
+	      (cons item results))))
     (if verify
-      (if (string-equal (concat (delq break-permitted (nreverse results)))
+      (if (string-equal (concat (delq hyph-hack/break-permitted (nreverse results)))
 		   string)
 	  (concat (nreverse results))
 	(error "Lost some stuff here: %s vs %s" string
-	       (concat (delq break-permitted (nreverse results)))))
+	       (concat (delq hyph-hack/break-permitted (nreverse results)))))
       (concat (nreverse results)))))
+
+(defun hyph-hack/hyphenate-long-string (string)
+  "Insert character 130 (break permitted here) into STRING.
+
+This function tries to break suspiciously long words that LaTeX
+might crash on.  To change length of string to work on, check
+`hyph-hack/long-word-regex'."
+  (let ((end-of-match 1)
+	(case-fold-search nil)
+	results)
+    (with-temp-buffer
+      (insert string)
+      (goto-char (point-min))
+      (while (re-search-forward hyph-hack/long-word-regex nil t)
+	(setq end-of-match (match-end 0))
+	(goto-char (match-beginning 0))
+	(while (re-search-forward hyph-hack/vowels-iast-regex end-of-match t)
+	  (insert hyph-hack/break-permitted)
+	  (setq end-of-match (+ end-of-match 1)))
+	(goto-char end-of-match)
+	(setq end-of-match nil))
+      (buffer-substring-no-properties (point-min) (point-max)))))
 
 ;; (message "argv: %s" argv)
 
-(defun hyphenate-help ()
+(defun hyph-hack/help ()
   (message "
 This script inserts hyphenation marks (BREAK
 PERMITTED HERE) in unicode devanāgarī text, to help with
@@ -59,23 +87,28 @@ It does not touch any other text, so, unless you have special
 constructs in devanāgarī (commands of some sort, or xml tags),
 the changes should not do much harm™.\n"))
 
-(cond
- ((= 1 (length argv))
-  (let (line)
-    (condition-case nil;; ignore stdin errors at the end
-	(while (setq line (read-from-minibuffer ""))
-	  (princ (hacks/devanāgarī-hyphenate-string line))
-	  (princ "\n"))
-      (error nil))))
- ((and (= 2 (length argv)) (not (string= "-h" (cadr argv))))
-  (let ((file (expand-file-name (cadr argv))))
-    (if (file-readable-p file)
-	(with-temp-buffer
-	  (insert-file-contents file)
-	  (princ (hacks/devanāgarī-hyphenate-string
-		  (delete-and-extract-region (point-min) (point-max)))))
-      (error "Can't open file %s" file))))
- ((and (cdr argv) (string= "-h" (cadr argv)) (hyphenate-help)))
- (t (error (hyphenate-help))))
+(when noninteractive
+  (cond
+   ((= 1 (length argv))
+    (let (line)
+      (condition-case nil ;; ignore stdin errors at the end
+	  (while (setq line (read-from-minibuffer ""))
+	    (princ (hyph-hack/hyphenate-long-string
+		    (hyph-hack/devanāgarī-hyphenate-string line)))
+	    (princ "\n"))
+	(error nil))))
+   ((and (= 2 (length argv)) (not (string= "-h" (cadr argv))))
+    (let ((file (expand-file-name (cadr argv))))
+      (if (file-readable-p file)
+	  (with-temp-buffer
+	    (insert-file-contents file)
+	    (princ (hyph-hack/hyphenate-long-string
+		    (hyph-hack/devanāgarī-hyphenate-string
+		     (delete-and-extract-region (point-min) (point-max))))))
+	(error "Can't open file %s" file))))
+   ((and (cdr argv) (string= "-h" (cadr argv)) (hyph-hack/help)))
+   (t (error (hyph-hack/help))))
+  (setq argv nil))
 
-(setq argv nil)
+
+(provide 'hyph-hack)
